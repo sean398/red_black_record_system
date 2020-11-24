77b1992a51cf5d6f80702ee8b6a48da3b60f4f0d
sean398 <guohaochuan5461@gmail.com>
2020-11-24 22:03:06 +0800

    udpate

--- src/components/selectButton/SelectCenter.tsx
+++ src/components/selectButton/SelectCenter.tsx
@@ -7,7 +7,7 @@ interface coordinater {
 }
 
 const SelectCenter = () => {
-    const [group, setGroup] = useState<number>(3);
+    const [group, setGroup] = useState<number>(5);
     const gameArray = [];
 
     for (let i = 0; i < group; i++) {
@@ -19,22 +19,51 @@ const SelectCenter = () => {
 
     return (
         <div className="select-center-container">
-            {gameArray.map((row) => {
-                return (
-                    <div
-                        className="select-center-row"
-                        style={{
-                            height: `${100 / group}%`
-                        }}
-                    >
-                        {row.map((col) => {
-                            return (
-                                <div className="select-center-button">{`${col.x}+${col.y}`}</div>
-                            );
-                        })}
-                    </div>
-                );
-            })}
+            <div className="select-center-top-title-container">
+                {gameArray.map((row, index) => {
+                    return (
+                        <div className="select-center-title-item">{`第${
+                            index + 1
+                        }组`}</div>
+                    );
+                })}
+            </div>
+            <div className="select-center-main-container">
+                <div className="select-center-top-left-container">
+                    {gameArray.map((row, index) => {
+                        return (
+                            <div
+                                style={{
+                                    height: `${100 / group}%`
+                                }}
+                                className="select-center-title-item"
+                            >{`第${index + 1}组`}</div>
+                        );
+                    })}
+                </div>
+                <div className="select-center-content">
+                    {gameArray.map((row) => {
+                        return (
+                            <div
+                                className="select-center-row"
+                                style={{
+                                    height: `${100 / group}%`
+                                }}
+                            >
+                                <>
+                                    {row.map((col) => {
+                                        return (
+                                            <>
+                                                <div className="select-center-button">{`${col.x}+${col.y}`}</div>
+                                            </>
+                                        );
+                                    })}
+                                </>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 };
